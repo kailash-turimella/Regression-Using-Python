10c3fae31bdba205a39c9a6ec99b948b236ce442
Kailash Turimella <kailash.turimella@gmail.com>
2020-07-29 13:07:06 -0700

    Update simple_linear_regression.py

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -16,13 +16,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing datasets
+# Make sure that the 'Salary_Data.csv' file is present in the working directory.
 dataset = pd.read_csv('Salary_Data.csv') # Dataset
 X = dataset.iloc[:,:-1].values           # Independant Variables
 y = dataset.iloc[:,1].values             # Dependant Variables
 
 # Splitting the data set into training set and test set
-from sklearn.model_selection import train_test_split 
-X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 1/3) # Dividing into training and test sets
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 1/3) # Dividing into training and test sets(Test set = 1/3 of the dataset)
 
 # Fitting the Simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
@@ -48,7 +49,6 @@ plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
 
-# Evaluating Model Performance
-#    Higher the better
+# Evaluating Model Performance(Higher the better)
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_pred)
