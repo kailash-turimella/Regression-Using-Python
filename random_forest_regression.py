85c81c44b18e4d5c072df151a667002c1560d487
Kailash Turimella <kailash.turimella@gmail.com>
2020-08-02 16:33:50 -0700

    Update random_forest_regression.py

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -1,8 +1,6 @@
 """
 Random Forest Regression
 
-Ensemble Learning - When you take multiple machine learning algorithms and combine them.
-
 Random Forest is a type of ensemble learning
 Ensemble learning is when you take multiple algorithms and put them together to make something more powerful
    or the same algorithm multiple times
@@ -15,8 +13,7 @@ You will have multiple decision trees
 The prediction = the average value of predictions made by each decision tree
 
 Instead of getting one prediction, like in decision tree regression, you are getting multiple predictions
- and that improves the accuracy
- for example if a tree doesn't turn out to be as good it would not ruin the prediction as we are taking the average
+ which improves the accuracy
  
 Ensemble algorithms are more stable
  because any changes in the dataset could impact one tree but for them to impact multiple tree is harder
@@ -41,7 +38,7 @@ y_pred = regressor.predict([[6.5]])
 
 # Visualising Regression Results(Higher resolution)
 X_grid = np.arange(min(X),max(X),0.01)     #all values of X incrimented by 0.1(X_grid = 1,1.1,1.2,1.3...)
-X_grid = X_grid.reshape((len(X_grid), 1)) #convert into a matrix
+X_grid = X_grid.reshape((len(X_grid), 1))  #convert into a matrix
 plt.scatter(X,y,color = 'red')
 plt.plot(X_grid,regressor.predict(X_grid),color = 'blue')
 plt.title('Random Forest Regression Model')
